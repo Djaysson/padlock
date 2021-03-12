e3488a76af69fc3b70ff623220c8b26f9463e203
Djaysson <djayson.dev@gmail.com>
2021-03-12 20:04:35 -0300

    Starting the creation of the app interface

--- App.js
+++ App.js
@@ -1,18 +1,10 @@
-import React from 'react'
-import { Text, View, StyleSheet } from 'react-native'
+import React from 'react';
+import { View } from 'react-native';
+import Padlock from './src/page/Padlock';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Padlock</Text>
-    </View>
+    <Padlock />
   )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
-})
---
+++ src/page/Padlock.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import { View, Text, Image, StyleSheet } from 'react-native'
+import Slider from '@react-native-community/slider';
+
+export default function Padlock() {
+  return (
+    <View style={styles.container}>
+      <Image
+        source={require('../../assets/logo.png')}
+        style={styles.logo}
+      />
+      <Text style={styles.title}>12 Caracteres</Text>
+      <View style={styles.area}>
+        <Slider
+          style={{ height: 50 }}
+          minimumValue={5}
+          maximumValue={15}
+          minimumTrackTintColor="#FF0000"
+          maximumTrackTintColor="#000"
+
+        />
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#F3F3FF'
+  },
+  logo: {
+    marginBottom: 60
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold'
+  },
+  area: {
+    marginBottom: 15,
+    marginTop: 15,
+    backgroundColor: '#FFF',
+    width: '90%',
+    borderRadius: 7
+  }
+})
